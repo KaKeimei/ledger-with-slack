e251341922b2c350d72d1f0e05508d5599b42a52
KaKeimei <hqm237594742@gmail.com>
2020-11-23 13:45:39 +0900

    add last month

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,9 @@ ledger -f $FILE_PATH balance assets liabilities >> result.txt
 # echo "==========Total Expenses========\n" >> result.txt
 # ledger -f $FILE_PATH balance expenses >> result.txt
 
-echo "===This Month==\n" >> result.txt
-ledger -p "this month" --cleared-format="%(justify(scrub(get_at(display_total, 0)), 16, 16 + int(prepend_width),true, color))  %-(ansify_if(partial_account(options.flat), blue if color))\n\n" cleared Expenses:日元 -f $FILE_PATH >> result.txt
+echo "\n===Expenses Last Month==\n" >> result.txt
+ledger -p "last month" --cleared-format="%(justify(scrub(get_at(display_total, 0)), 16, 16 + int(prepend_width),true, color))  %-(ansify_if(partial_account(options.flat), blue if color))\n" cleared Expenses:日元 -f $FILE_PATH >> result.txt
+
+echo "\n===Expenses This Month==\n" >> result.txt
+ledger -p "this month" --cleared-format="%(justify(scrub(get_at(display_total, 0)), 16, 16 + int(prepend_width),true, color))  %-(ansify_if(partial_account(options.flat), blue if color))\n" cleared Expenses:日元 -f $FILE_PATH >> result.txt
 cat result.txt | ./slackcat -u $SLACK_WEBHOOK_URL
