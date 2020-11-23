511e52a2ae7db984436cee0e315572b2fbab7bc1
KaKeimei <hqm237594742@gmail.com>
2020-11-23 13:37:24 +0900

    reformat

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ usage:
 ```
  docker run --rm -v $(pwd):/data ledger-with-slack -u $SLACK_WEBHOOK_URL -f /data/$LEDGER_FILE
 ```
+
+cleared format:
+```
+"%(justify(scrub(get_at(display_total, 0)), 16, 16 + int(prepend_width),true, color))  %-(ansify_if(partial_account(options.flat), blue if color))\n\n"
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,6 @@ ledger -f $FILE_PATH balance assets liabilities >> result.txt
 # echo "==========Total Expenses========\n" >> result.txt
 # ledger -f $FILE_PATH balance expenses >> result.txt
 
-echo "==========Expenses This Month========\n" >> result.txt
-ledger -p "this month" cleared Expenses:日元 -f $FILE_PATH >> result.txt
+echo "===This Month==\n" >> result.txt
+ledger -p "this month" --cleared-format="%(justify(scrub(get_at(display_total, 0)), 16, 16 + int(prepend_width),true, color))  %-(ansify_if(partial_account(options.flat), blue if color))\n\n" cleared Expenses:日元 -f $FILE_PATH >> result.txt
 cat result.txt | ./slackcat -u $SLACK_WEBHOOK_URL
